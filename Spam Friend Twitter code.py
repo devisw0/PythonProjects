093742119e87c9caf283cc4eff361b05904088a4
Devan P <91017674+devisw0@users.noreply.github.com>
2022-02-27 22:40:53 -0500

    Update Spam Friend Twitter code.py

--- Spam Friend Twitter code.py
+++ Spam Friend Twitter code.py
@@ -2,8 +2,8 @@ from telnetlib import STATUS
 import tweepy
 import time  
 
-auth = tweepy.OAuthHandler("qViOysKXR3lTFEMwBdlEUKVFS","6KsTNhw89ioLUYgExg82KpDGo2b015zOHwEGAUsCcf1x0YwEVq")
-auth.set_access_token("1471941803519664137-nYHuxKPfdodiovYLR1Q9OloJZjAUqn","2qofxnh4XghqqY4RenW7ofBH0OaRRxrqIRQP4z4JHdiuL")
+auth = tweepy.OAuthHandler("insert key1, insert key 2")
+auth.set_access_token("insert other keys 1, and 2")
 
 api = tweepy.API(auth)
 tweet = "@InsertFriend'sUser Hi!"
